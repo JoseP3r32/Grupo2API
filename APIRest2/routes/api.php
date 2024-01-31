6ad69734e30c3ec143cbccb3191e82f92fa5cf07
JoseP3r32 <146430742+JoseP3r32@users.noreply.github.com>
2024-01-31 12:10:08 +0100

    Llamadas Register, Login y User

--- APIRest2/routes/api.php
+++ APIRest2/routes/api.php
@@ -5,18 +5,15 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\RegisterController;
 use App\Http\Controllers\API\EmotionController;
+use App\Http\Controllers\API\UserController;
+
 
 Route::controller(RegisterController::class)->group(function(){
     Route::post('register', 'register');
     Route::post('login', 'login');
 });
 
-Route::middleware('auth:sanctum')->group( function () {
-// La siguiente ruta devuelve las ofertas
-    Route::get('/emotions', [EmotionController::class, 'index']); 
-});
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/emotions', [EmotionController::class, 'index']);
+    Route::get('/users', [UserController::class, 'index']); // Ruta para obtener usuarios
 });
